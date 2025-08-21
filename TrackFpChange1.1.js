96a49feeeb9d5e84b9bfc9be36cf81768f7168e6
AdentyPro <support@adentypro.com>
2025-08-21 11:35:14 +0000

    try semaphore to avoid cookie 4 requests

--- TrackFpChange1.1.js
+++ TrackFpChange1.1.js
@@ -15,81 +15,105 @@ setTimeout(async () => {
   let fpPVCount;
   let sCookiefpPVCountVal;
 
-  if (!window.aidpSCookieList) {
-    window.aidpSCookieList = await window.adenty?.scookie?.get();
+  if (window.aidpSCookieListPromise) {
+    window.aidpSCookieListPromise.then(useData).catch(console.error);
   }
 
-  try {
-    fp = window.aidpSCookieList?.find(i => i.name === fpName)?.value; 
-  } catch (e) {
-    fp = null;
-  }
+  let resolveFn, rejectFn;
+  window.aidpSCookieListPromise = new Promise((resolve, reject) => {
+    resolveFn = resolve;
+    rejectFn = reject;
+  });
 
-  try {
-    fpPVCount = window.aidpSCookieList?.find(i => i.name === fpPVCountName);
-    sCookiefpPVCountVal = Number(fpPVCount.value);
-  } catch (e) {
-    fpPVCount = null;
-    sCookiefpPVCountVal = null;
-  }
-
-
-
-
-  const fpData = window.adenty?.dl?.adenty?.visit?.rid
- 
-  
-  
-
-  
-  
-  let newfpPVCount
-  if (!sCookiefpPVCountVal || !fp) {
-    window.adenty.scookie.set({
-      name: fpName,
-      value: fpData,
-      expires: date.toISOString(),
+  if (window.aidpSCookieList) {
+    resolveFn(window.aidpSCookieList);
+  } else {
+    window.adenty?.scookie?.get()
+    .then(data => {
+      resolveFn(data);
+    })
+    .catch(error => {
+      resolveFn([]);
     });
+  }
+ 
+  window.aidpSCookieListPromise.then(useData).catch(console.error);
+
+  function useData(data) {
+    window.aidpSCookieList = data;
+    processData();
+  }
+
+  function processData() {
+    try {
+      fp = window.aidpSCookieList?.find(i => i.name === fpName)?.value; 
+    } catch (e) {
+      fp = null;
+    }
+
+    try {
+      fpPVCount = window.aidpSCookieList?.find(i => i.name === fpPVCountName);
+      sCookiefpPVCountVal = Number(fpPVCount.value);
+    } catch (e) {
+      fpPVCount = null;
+      sCookiefpPVCountVal = null;
+    }
+
+
+
+
+    const fpData = window.adenty?.dl?.adenty?.visit?.rid
+  
+    
+    
+
+    
+    
+    let newfpPVCount
+    if (!sCookiefpPVCountVal || !fp) {
+      window.adenty.scookie.set({
+        name: fpName,
+        value: fpData,
+        expires: date.toISOString(),
+      });
+      window.adenty.scookie.set({
+        name: fpPVCountName,
+        value: JSON.stringify(1),
+        expires: date.toISOString(),
+      });
+      return;
+    }
+
+    
+    
+    
+    if (fp !== fpData) {
+      newfpPVCount = 1;
+    sCookiefpPVCountVal = (sCookiefpPVCountVal ? sCookiefpPVCountVal: 0) //TODO check when SQL querying whether we have 0 in events, this is not expected
+      // window.adenty.event.fireevent({
+        // name: 'VisitorFPChanged', 
+        // eventarguments: JSON.stringify({[fpName]: fpData})
+      // });
+      window.adenty.event.fireevent({ 
+        name: 'VisitorFPCountChanged',
+        eventarguments: JSON.stringify({[fpPVCountName]: sCookiefpPVCountVal, [fpName]: fpData})
+      });
+
+      window.adenty.scookie.set({
+        name: fpName,
+        value: fpData,
+        //expires: date.toISOString(), // TODO: make sure that here we do not set to NULL expiredate
+      });
+    }
+    else {
+    newfpPVCount = (sCookiefpPVCountVal ? sCookiefpPVCountVal + 1 : 1);
+    }
+
     window.adenty.scookie.set({
       name: fpPVCountName,
-      value: JSON.stringify(1),
-      expires: date.toISOString(),
-    });
-    return;
-  }
-
-  
-  
-  
-  if (fp !== fpData) {
-    newfpPVCount = 1;
-	sCookiefpPVCountVal = (sCookiefpPVCountVal ? sCookiefpPVCountVal: 0) //TODO check when SQL querying whether we have 0 in events, this is not expected
-    // window.adenty.event.fireevent({
-      // name: 'VisitorFPChanged', 
-      // eventarguments: JSON.stringify({[fpName]: fpData})
-    // });
-    window.adenty.event.fireevent({ 
-      name: 'VisitorFPCountChanged',
-      eventarguments: JSON.stringify({[fpPVCountName]: sCookiefpPVCountVal, [fpName]: fpData})
-    });
-
-    window.adenty.scookie.set({
-      name: fpName,
-      value: fpData,
+      value: JSON.stringify(newfpPVCount),
       //expires: date.toISOString(), // TODO: make sure that here we do not set to NULL expiredate
-    });
+    }); 
   }
-  else {
-	newfpPVCount = (sCookiefpPVCountVal ? sCookiefpPVCountVal + 1 : 1);
-  }
-
-  window.adenty.scookie.set({
-    name: fpPVCountName,
-    value: JSON.stringify(newfpPVCount),
-    //expires: date.toISOString(), // TODO: make sure that here we do not set to NULL expiredate
-  }); 
-
-
-
-
+  
 }, 0);
