702cd5064b9fa690401b3a331da21d34826f3511
AdentyPro <support@adentypro.com>
2026-04-28 14:51:42 +0000

    bdn ls demeter

---
+++ lsdem.js
@@ -0,0 +1,45 @@
+(async function() {
+    function isEmptyObj(obj) {
+        return isEmpty(obj?.t, sumValue, 1) && obj?.h?.every(inner => isEmpty(inner, sumValue, 0));
+    }
+    function isEmpty(data, func, max) {
+        const sum = data?.reduce(func, 0);
+        return sum <= max;
+    }
+    function sumValue(sum, value) {
+        return sum + (Array.isArray(value) ? 0 : value);
+    }
+    const cKey = 'lsdem';
+    async function restoreFromSCookie() {
+        let cVal;
+        try {
+            cVal = await adenty.scookie.get(cKey);
+        } catch (e) {
+            cVal = null;
+        }
+        if(cVal && cVal.value) {
+            //console.log('update demeter');
+            localStorage.demeter = cVal.value;
+        }
+    }
+    const dem = localStorage.demeter;
+    if(dem) {
+        let decVal;
+        let obj;
+        try {
+            decVal = adenty.tools.base64.decode(dem);
+            obj = JSON.parse(decVal);
+        } catch (e) {
+            await restoreFromSCookie();
+            return;
+        }
+        if(!isEmptyObj(obj)) {
+            adenty.scookie.set({"name" : cKey, "value" : dem});
+            //console.log('set cookie');
+        } else {
+            await restoreFromSCookie();
+        }
+    } else {
+        await restoreFromSCookie();
+    }
+})();
