a3af4a9fe6a0cc18041eddd0850e126e2d2b44c3
AdentyPro <support@adentypro.com>
2026-07-15 10:29:05 +0000

    Add files via upload

--- test-adenty-dl.js
+++ test-adenty-dl.js
@@ -1,5 +1,5 @@
-( async function () {
+(async function () {
     await adenty?.dl;
     console.log("Check adenty dl await. VID: ", adenty?.dl?.adenty?.visit?.vid);
     console.log("Check adenty dl await. IPSHA: ", adenty?.dl?.adenty?.visit?.ipsha);
-})
+})()
