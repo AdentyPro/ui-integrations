39cebda03ad39e8bd7648a45b64d49410b2faf9a
AdentyPro <support@adentypro.com>
2026-07-15 09:57:59 +0000

    TEST await adenty dl

---
+++ test-adenty-dl.js
@@ -0,0 +1,5 @@
+( async function () {
+    await adenty?.dl;
+    console.log("Check adenty dl await. VID: ", adenty?.dl?.adenty?.visit?.vid);
+    console.log("Check adenty dl await. IPSHA: ", adenty?.dl?.adenty?.visit?.ipsha);
+})
